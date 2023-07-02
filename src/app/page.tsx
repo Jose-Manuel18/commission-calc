fcc5ab4770e5f027db7567168219fa18202c1373
Jose-Manuel18 <103284630+Jose-Manuel18@users.noreply.github.com>
2023-07-01 23:27:46 -0400

    ui changes to input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { CommissionCalculator } from "@/components/Commission"
-
+export const metadata = {
+  title: "Commission",
+  description: "Commission calculator",
+}
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
--- src/components/Commission.tsx
+++ src/components/Commission.tsx
@@ -1,16 +1,18 @@
 "use client"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { motion } from "framer-motion"
 
 export function CommissionCalculator() {
-  const [sales, setSales] = useState(0)
+  const [sales, setSales] = useState<number>(0)
   const [commissionRate, setCommissionRate] = useState(0)
   const [commission, setCommission] = useState(0)
-
+  const salesRef = useRef<HTMLInputElement>(null)
+  const commissionRef = useRef<HTMLInputElement>(null)
   const calculateCommission = () => {
     const commission = (sales * commissionRate) / 100
     setCommission(commission)
   }
+  console.log(sales)
 
   return (
     <motion.div
@@ -28,13 +30,38 @@ export function CommissionCalculator() {
             >
               Sales Amount ($)
             </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="sales"
-              type="number"
-              placeholder="Sales amount"
-              onChange={(e) => setSales(parseInt(e.target.value))}
-            />
+            <div className="flex items-center justify-center shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              <input
+                ref={salesRef}
+                className="w-full focus:outline-none focus:shadow-outline"
+                id="sales"
+                type="number"
+                placeholder="Sales amount"
+                onChange={(e) => setSales(parseInt(e.target.value))}
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className={`flex ml-2 w-4 h-4 cursor-pointer text-black ${
+                  sales === 0 ? "hidden" : "block"
+                }`}
+                onClick={() => {
+                  setSales(0)
+                  if (salesRef.current) {
+                    salesRef.current.value = ""
+                  }
+                }}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </div>
           </div>
           <div className="mb-6">
             <label
@@ -43,13 +70,38 @@ export function CommissionCalculator() {
             >
               Commission Rate (%)
             </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="commission-rate"
-              type="number"
-              placeholder="Commission rate"
-              onChange={(e) => setCommissionRate(parseInt(e.target.value))}
-            />
+            <div className="flex items-center justify-center shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              <input
+                ref={commissionRef}
+                className=" w-full focus:outline-none focus:shadow-outline"
+                id="commission-rate"
+                type="number"
+                placeholder="Commission rate"
+                onChange={(e) => setCommissionRate(parseInt(e.target.value))}
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className={`flex ml-2 w-4 h-4 cursor-pointer text-black ${
+                  commissionRate === 0 ? "hidden" : "block"
+                }`}
+                onClick={() => {
+                  setCommissionRate(0)
+                  if (commissionRef.current) {
+                    commissionRef.current.value = ""
+                  }
+                }}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </div>
           </div>
           <div className="flex items-center justify-between">
             <button
